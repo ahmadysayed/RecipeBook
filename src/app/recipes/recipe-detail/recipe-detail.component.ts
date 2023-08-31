bd38f332d7d128f04712833cb7bf9c00384746bb
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-08-31 19:20:43 +0200

    Passing Ingredients from Recipes to the Shopping List (via a Service)

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,12 +9,22 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
 
+  isDropdownOpen = false;
+
   @Input() recipe!: Recipe;
 
-  constructor(){}
+  constructor(private recipeService: RecipeService){}
 
   ngOnInit(){
     
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  onAddToShoppingList(){
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
 }
