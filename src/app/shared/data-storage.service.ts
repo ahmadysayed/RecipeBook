18a07d1e5bab51270083a151d037ab6b0e3277c9
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-09-24 16:13:31 +0200

    Resolving Data Before Loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -20,14 +20,15 @@ export class DataStorageService {
     }
 
     fetchRecipes(){
-         this.http.get<Recipe[]>('https://ng-course-recipe-book-746b8-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').pipe(
+        return this.http.get<Recipe[]>('https://ng-course-recipe-book-746b8-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').pipe(
             map(recipes => {
                 return recipes.map(recipe => {
                     return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []}
                 })
+            }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);  
             })
-         ).subscribe(recipes => {
-            this.recipeService.setRecipes(recipes);
-         })
+         )
     }
 }
